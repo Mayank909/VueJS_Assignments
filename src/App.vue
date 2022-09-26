ee98647c853dd8039672eaa2697d3c25707d1946
Mayank Verma <vermayank909@gmail.com>
2022-09-26 16:27:44 +0530

    Added vuetify into the restaurant app

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
+<script>
 
+export default {
+  name: 'App',
 
-nav {
-  padding: 30px;
+  data: () => ({
+    //
+  }),
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
