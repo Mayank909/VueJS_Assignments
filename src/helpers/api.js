9f401189a3db966940cf414a3c49d76295da323f
Mayank Verma <vermayank909@gmail.com>
2022-10-21 11:43:00 +0530

    Improved code of sortByColumn api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -21,73 +21,56 @@ export default class Services {
   async get(collectionName, id) {
     let data;
     await db
-      .collection(collectionName).doc(id)
+      .collection(collectionName)
+      .doc(id)
       .get()
       .then((snapshot) => {
         data = snapshot.data();
       });
-   return data;
+    return data;
   }
-  async delete(collectionName,id) {
+  async delete(collectionName, id) {
     await db.collection(collectionName).doc(id).delete();
   }
-  async put(collectionName,id, payload) {
+  async put(collectionName, id, payload) {
     await db.collection(collectionName).doc(id).update(payload);
   }
-  sortData(clickValue, headers, tableData){
+  sortData(clickValue, headers, tableData) {
+    for (let header of headers) {
+      header.sortable = false;
+    }
     switch (clickValue.value) {
-        case "name":
-          if(headers[4].sortable ){
-            headers[4].sortable = false;
-          }
-          if(headers[3].sortable){
-            headers[3].sortable = false;
-          }
-          clickValue.sortable = true;
-          tableData.sort((first, second) =>
-            first.name.toLowerCase() < second.name.toLowerCase() ? -1 : 1
-          );
-          // -1 for not require to sort
-          // 1 for require a sort
-          break;
-        case "active":
-           if(headers[1].sortable){
-            headers[1].sortable = false;
-          }
-          clickValue.sortable = true;
-          tableData.sort((element) => (element.isActive ? -1 : 1));
-          // -1 for not require to sort
-          // 1 for require a sort
-          break;
-        case "price":
-          if(headers[1].sortable ){
-            headers[1].sortable = false;
-          }
-          if(headers[4].sortable){
-            headers[4].sortable = false;
-          }
-          clickValue.sortable = true;
-            tableData.sort((first, second) => first.price - second.price);
-            // -1 for not require to sort
-            // 1 for require a sort
-            break;
-        case "discount_price":
-          if(headers[1].sortable ){
-            headers[1].sortable = false;
-          }
-          if(headers[3].sortable){
-            headers[3].sortable = false;
-          }
-          clickValue.sortable = true;
-            tableData.sort(
-              (first, second) => first.discount_price - second.discount_price
-            );
-            // -1 for not require to sort
-            // 1 for require a sort
-            break;
-        default:
-          break;
+      case "name":
+        clickValue.sortable = true;
+        tableData.sort((first, second) =>
+          first.name.toLowerCase() < second.name.toLowerCase() ? -1 : 1
+        );
+        // -1 for not require to sort
+        // 1 for require a sort
+        break;
+      case "active":
+        clickValue.sortable = true;
+        tableData.sort((element) => (element.isActive ? -1 : 1));
+        // -1 for not require to sort
+        // 1 for require a sort
+        break;
+      case "price":
+        clickValue.sortable = true;
+        tableData.sort((first, second) => first.price - second.price);
+        // -1 for not require to sort
+        // 1 for require a sort
+        break;
+      case "discount_price":
+        clickValue.sortable = true;
+        tableData.sort(
+          (first, second) => first.discount_price - second.discount_price
+        );
+        // -1 for not require to sort
+        // 1 for require a sort
+        break;
+      default:
+        break;
     }
     return tableData;
-    }
+  }
 }
