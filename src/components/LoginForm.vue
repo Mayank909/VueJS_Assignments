d2e0bed5e027accbf756c8d8d56fe93f6839fb2a
Mayank Verma <vermayank909@gmail.com>
2022-10-27 19:20:46 +0530

    Created and Implemented LoginForm component.

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Login</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  color="primary"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  color="primary"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="$emit('closeDialog')">
+                Cancel
+              </v-btn>
+              <v-btn color="primary" text @click="$emit('login')">
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  data: () => ({
+    dialog: true,
+  }),
+};
+</script>
+<style></style>
