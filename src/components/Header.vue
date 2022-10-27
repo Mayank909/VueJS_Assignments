924d833c96c207b830bb1ed84e2e12ff79b681b1
Mayank Verma <vermayank909@gmail.com>
2022-10-27 19:12:06 +0530

    Implemented login and signUp button form dialog functionality.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,8 +16,8 @@
 
     <v-tabs align-with-title>
       <v-btn>Home</v-btn>
-      <v-btn>Login</v-btn>
-      <v-btn>Sign Up</v-btn>
+      <v-btn :color="btnColorLogin" dark @click="displayLogin()"> Login </v-btn>
+      <v-btn :color="btnColorSignUp" dark @click="displaySignUp()"> Sign Up </v-btn>
     </v-tabs>
 
     <template v-slot:append>
@@ -28,11 +28,58 @@
         >
       </v-btn>
     </template>
+    <component :is="loginComponent" v-if="loginClick" @login="loginSuccess"
+          @closeDialog="closeLogin" />
+    <component :is="signUpComponent" v-if="signUpClick" @signUp="signUpSuccess"
+          @closeDialog="closeSignUp" />
   </v-app-bar>
 </template>
 
 <script>
-export default {};
+import { defineAsyncComponent } from "vue";
+export default {
+  data() {
+    return {
+      loginComponent: "",
+      loginClick: false,
+      signUpClick: false,
+      btnColorLogin: "",
+      btnColorSignUp: "",
+    };
+  },
+  components: {
+    LoginForm: defineAsyncComponent(() => import("./LoginForm.vue")),
+    SignUpForm: defineAsyncComponent(() => import("./SignUpForm.vue")),
+  },
+  methods: {
+    displayLogin() {
+      this.btnColorLogin = "primary";
+      this.loginComponent = "LoginForm";
+      this.loginClick = true;
+    },
+    loginSuccess(){
+      this.loginClick = false;
+      this.btnColorLogin =""
+    },
+    closeLogin(){
+      this.loginClick = false;
+      this.btnColorLogin = ""
+    },
+     displaySignUp() {
+      this.btnColorSignUp = "primary";
+      this.signUpComponent = "signUpForm";
+      this.signUpClick = true;
+    },
+     signUpSuccess(){
+      this.signUpClick = false;
+      this.btnColorSignUp =""
+    },
+    closeSignUp(){
+      this.signUpClick = false;
+      this.btnColorSignUp = ""
+    },
+  },
+};
 </script>
 
 <style>
